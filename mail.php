72335380a1eea445a08a0afe56261c757376fd24
VANDEMATARAM <46159712+VANDEMATARAM@users.noreply.github.com>
2020-04-22 12:22:29 +0530

    update mail.php 2.0.6

--- mail.php
+++ mail.php
@@ -5,13 +5,7 @@ $subject = "Registration";
 
 $body= $_REQUEST['name'];
 $body= $_REQUEST['email'];
-$body= $_REQUEST['dob'];
-$body= $_REQUEST['age'];
-$body= $_REQUEST['male'];
-$body= $_REQUEST['female'];
-$body= $_REQUEST['transgender'];
-$body= $_REQUEST['phone'];
-$body= $_REQUEST['college'];
+
 
 $ma = $_REQUEST['email'];
 $header = "From:$ma";
